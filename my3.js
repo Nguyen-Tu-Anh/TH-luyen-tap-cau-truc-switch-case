809a062479807dc2ff5e59583ca54cea3ee01b52
Nguyen-Tu-Anh <nguyentuanh.10t2@gmail.com>
2021-10-08 14:50:06 +0700

    first commit

---
+++ my3.js
@@ -0,0 +1,32 @@
+let number = +prompt('Nhap so can kiem tra');
+switch (number){
+    case -5:
+        alert(-1);
+        break;
+    case -4:
+        alert(-1);
+        break;
+    case -3:
+        alert(-1);
+        break;
+    case -2:
+        alert(-1);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+        alert(1);
+        break;
+    case 3:
+        alert(1);
+        break;
+    case 4:
+        alert(1);
+        break;
+    case 5:
+        alert(1);
+        break;
+    default:
+        alert(0)
+}
